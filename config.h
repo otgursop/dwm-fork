9e3fb35d88c229fb04cf42c29769eaaa2dde9411
otgursop <gursteklup@tutamail.com>
2025-09-09 15:08:19 +0500

    config.h updated

--- config.h
+++ config.h
@@ -1,14 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* APPEARANCE */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 5;       /* snap pixel */
 
 // VANITYGAPS_PATCH
-static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 2;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 2;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 2;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 
 static const int showbar            = 1;        /* 0 means no bar */
@@ -21,8 +21,8 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 
-static const char *fonts[]          = { "JetBrainsMonoNL Nerd Font:style=ExtraBold:size=11" };
-static const char dmenufont[]       = "JetBrainsMonoNL Nerd Font:style=ExtraBold:size=11";
+static const char *fonts[]          = { "MonaspiceKr Nerd Font:style=Medium:size=10.35" };
+static const char dmenufont[]       = "MonaspiceKr Nerd Font:style=Medium:size=10.35";
 
 // COLORS
 static const char col_gray1[]       = "#222222";
@@ -31,18 +31,20 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#435144"; //475933
 
+static const char col_panel[]       = "#151614";
+
 static const char col_border_idle[]	= "#262d26"; //2e382e
 static const char col_border_act[]  = "#435144";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_border_idle },
+	[SchemeNorm] = { col_gray3, col_panel, col_border_idle },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_border_act  },
 };
 
 
 /* TAGS */
-static const char *tags[] = { "a", "b", "c", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "m", "b", "t", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -85,15 +87,16 @@ static const Layout layouts[] = {
 // VOLUME MACROS
 #include <X11/XF86keysym.h>
 
+
 /* USER COMMANDS */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", ">>", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_panel, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", ">>", NULL };
 static const char *screenshot[] = { "flameshot", "gui", NULL };
-static const char *lock[] = { "i3lock", "-i", "/home/hwd/Pictures/Wallpapers/i3lock/i3lock3.png", "-t", "-u", NULL };
+static const char *lock[] = { "slock", NULL };
 
 static const char *termcmd[]  = { "st", NULL };
-static const char *librewolf[] = { "librewolf", NULL };
-static const char *thunar[] = { "thunar", NULL };
+static const char *browser[] = { "firefox", NULL };
+static const char *filemanager[] = { "thunar", NULL };
 
 /* KEY MODIFIERS */
 static const Key keys[] = {
@@ -104,8 +107,8 @@ static const Key keys[] = {
 	{ 0,							XK_Print,  spawn,		   {.v = screenshot } },
 	{ MODKEY,						XK_s,	   spawn,		   {.v = lock } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,				XK_b,	   spawn,		   {.v = librewolf } },
-	{ MODKEY|ShiftMask,				XK_e,	   spawn,	       {.v = thunar } },
+	{ MODKEY|ShiftMask,				XK_b,	   spawn,		   {.v = browser } },
+	{ MODKEY|ShiftMask,				XK_e,	   spawn,	       {.v = filemanager } },
 
 	/* DWMBLOCKS SIGNALS */
 	{ ShiftMask,					XK_Alt_L,  spawn,		   SHCMD("pkill -RTMIN+1 dwmblocks") },
@@ -128,22 +131,22 @@ static const Key keys[] = {
 	{ MODKEY|ControlMask,           XK_k,      inplacerotate,  {.i = -2} },
 
 	// VANITYGAPS_PATCH
-	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+	// { MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	// { MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	// { MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+	// { MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+	// { MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+	// { MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_0,      togglegaps,     {0} },
 	{ MODKEY|ControlMask,			XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
+	// { MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+	// { MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+	// { MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+	// { MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+	// { MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+	// { MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	// { MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+	// { MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 
 	// TOGGLE BAR
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
